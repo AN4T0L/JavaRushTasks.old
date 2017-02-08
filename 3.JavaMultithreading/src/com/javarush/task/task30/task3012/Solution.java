8b2b613f435e3ff626f59e059fbfe0c1fdb558b7
AN4TOL <lifter04a@gmail.com>
2017-02-08 08:17:05 +0300

    update AGAIN

---
+++ 3.JavaMultithreading/src/com/javarush/task/task30/task3012/Solution.java
@@ -0,0 +1,16 @@
+package com.javarush.task.task30.task3012;
+
+/* 
+Получи заданное число
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        solution.createExpression(74);
+    }
+
+    public void createExpression(int number) {
+        System.out.println(Integer.toString(number,3));
+    }
+}
